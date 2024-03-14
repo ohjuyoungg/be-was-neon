b1650959878438605602788385b5a3a55fbfda78
ohjuyoungg <ejrm12@naver.com>
2024-03-14 21:19:17 +0900

    refactor: 1차 객체 분리 리팩토링 ContentType 클래스 분리

--- src/main/java/webserver/ContentType.java
+++ src/main/java/webserver/ContentType.java
@@ -1,2 +1,28 @@
-package webserver;public class ContentType {
+package webserver;
+
+public class ContentType {
+
+    public String getContentType(String url) {
+        String[] parts = url.split("\\.");
+        String extension = parts[parts.length - 1];
+        switch (extension) {
+            case "html":
+                return "text/html;charset=utf-8";
+            case "css":
+                return "text/css";
+            case "js":
+                return "application/javascript";
+            case "png":
+                return "image/png";
+            case "jpg":
+            case "jpeg":
+                return "image/jpeg";
+            case "gif":
+                return "image/gif";
+            case "svg":
+                return "image/svg+xml"; // SVG 파일에 대한 Content-Type 추가
+            default:
+                return "application/octet-stream";
+        }
+    }
 }
