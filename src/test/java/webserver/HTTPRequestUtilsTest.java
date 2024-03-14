664bcb7cbd2102f41704f2fc2efd06fc76bcc3ec
ohjuyoungg <ejrm12@naver.com>
2024-03-14 11:51:45 +0900

    test: HTTPRequestUtils 클래스의 parseQueryString 메서드에 대한 테스트 추가 HTTPRequestUtils 클래스의 parseQueryString 메서드에 대한 단위 테스트를 추가

--- src/test/java/webserver/HTTPRequestUtilsTest.java
+++ src/test/java/webserver/HTTPRequestUtilsTest.java
@@ -1,11 +1,22 @@
 package webserver;
 
 import org.junit.jupiter.api.Test;
+import utils.HTTPRequestUtils;
 
-class HTTPRequestUtils {
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+class HTTPRequestUtilsTest {
 
     @Test
-
-
-
+    void testParseQueryString() {
+        String queryString1 = "userId=johndoe&password=password123&name=John+Doe&email=johndoe@example.com";
+        Map<String, String> params1 = HTTPRequestUtils.parseQueryString(queryString1);
+        assertThat(params1)
+                .containsEntry("userId", "johndoe")
+                .containsEntry("password", "password123")
+                .containsEntry("name", "John Doe")
+                .containsEntry("email", "johndoe@example.com");
+    }
 }
