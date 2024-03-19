432472ea117be4eaead9b552e77b733b5a935898
ohjuyoungg <ejrm12@naver.com>
2024-03-19 21:58:07 +0900

    feat: post로 메소드 변경 후 사용하지 않은 메소드 분리 후 isCreate 메소드 생성

--- src/main/java/webserver/httpMessage/HttpRequest.java
+++ src/main/java/webserver/httpMessage/HttpRequest.java
@@ -2,38 +2,43 @@ package webserver.httpMessage;
 
 import db.Database;
 import model.User;
-
 import utils.HttpRequestUtils;
-
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 public class HttpRequest {
 
-    private static String url;
+    private String url; // 인스턴스 변수로 변경
 
     public HttpRequest(String url) {
-        HttpRequest.url = url;
+        this.url = url; // 생성자에서 초기화
     }
 
-    public void registrationParamValue() {
-        if (url.startsWith("/registration/index.html?")) {
-            Map<String, String> params = parseParamsFromURL(url);
+    public boolean isCreateRequest() {
+        return url != null && url.startsWith("/create"); // null 체크 추가
+    }
+
+    public void registrationParamValue(OutputStream out, Map<String, String> params) throws IOException {
+        if (isCreateRequest()) { // isCreateRequest 메서드를 호출하여 null 체크
             User user = createUserFromParams(params);
             Database.addUser(user);
+            HttpResponse httpResponse = new HttpResponse();
+            httpResponse.response302Header(out);
         }
+    }
+
+    public static User createUserFromParams(Map<String, String> params) {
+        String userId = params.get("userId");
+        String password = params.get("password");
+        String name = params.get("name");
+        String email = params.get("email");
+        return new User(userId, password, name, email);
     }
 
     private Map<String, String> parseParamsFromURL(String url) {
         String[] parts = url.split("\\?");
         String queryString = parts[1];
         return HttpRequestUtils.parseQueryString(queryString);
-    }
-
-    private User createUserFromParams(Map<String, String> params) {
-        String userId = params.get("userId");
-        String password = params.get("password");
-        String name = params.get("name");
-        String email = params.get("email");
-        return new User(userId, password, name, email);
     }
 }
