d09083b6b4e138d7b5da390f77b3afbf1cc01395
ohjuyoungg <ejrm12@naver.com>
2024-03-19 21:56:45 +0900

    feat: contentLength 및 body 데이터 읽어오는 메소드 구현

--- src/main/java/webserver/MainHandler.java
+++ src/main/java/webserver/MainHandler.java
@@ -3,9 +3,13 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.HttpRequestUtils;
+import utils.IOUtils;
 import webserver.httpMessage.HttpRequest;
 import webserver.httpMessage.HttpResponse;
 
@@ -15,6 +19,8 @@ public class MainHandler implements Runnable {
     private static final Logger log = LoggerFactory.getLogger(MainHandler.class);
     private final Socket connection;
     private static String url;
+    private Map<String, String> parameters;
+
 
     public MainHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -27,20 +33,21 @@ public class MainHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            HttpRequest httpRequest = new HttpRequest(readRequestLog(br));
-            httpRequest.registrationParamValue();
+            HttpRequest httpRequest = new HttpRequest(readRequestLog(br, out));
             HttpResponse httpResponse = new HttpResponse();
 
-            // 요청된 파일을 읽어들이고 적절한 Content-Type 설정 후 응답
-            File file = new File("src/main/resources/static" + url);
+            httpRequest.registrationParamValue(out, parameters); // parameters 전달
 
+
+            // 정적 파일 요청을 처리합니다.
+            File file = new File("src/main/resources/static" + url);
             if (file.exists() && !file.isDirectory()) {
                 byte[] body = httpResponse.readFileContents(file);
                 ContentType contentType = new ContentType();
                 String type = contentType.getContentType(url);
-                httpResponse.sendResponse(out, body, type);
+                httpResponse.response200Header(out, body, type);
             } else {
-                // 파일이 존재하지 않는 경우 404 응답 반환
+                // 파일이 존재하지 않는 경우 404 응답을 보냅니다.
                 log.error("File not found: " + url);
                 httpResponse.send404NotFound(out);
             }
@@ -49,14 +56,34 @@ public class MainHandler implements Runnable {
         }
     }
 
-    private String readRequestLog(BufferedReader br) throws IOException {
+    private String readRequestLog(BufferedReader br, OutputStream out) throws IOException {
         String line = br.readLine();
         String[] tokens = line.split(" ");
+        Map<String, String> headers = new HashMap<>();
+
+        String requestBody = "";
+        String queryString = "";
 
         while (!"".equals(line)) {
             log.debug(line);
             line = br.readLine();
+            String[] headerTokens = line.split(": ", 2);
+
+            if (headerTokens.length == 2) {
+                headers.put(headerTokens[0], headerTokens[1]);
+            }
         }
+
+        if (tokens[1].contains("?")) {
+            queryString = tokens[1].substring(tokens[1].indexOf("?") + 1);
+            log.debug("query string:{}",queryString);
+        }
+
+        if (headers.get("Content-Length") != null) {
+            requestBody = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+        }
+        queryString = queryString + "&" + requestBody;
+        parameters = HttpRequestUtils.parseQueryString(queryString);
 
         log.info(Arrays.toString(tokens));
         url = tokens[1];
