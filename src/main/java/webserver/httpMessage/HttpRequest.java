0eb7ac2b460f8380953e4dad50590c34fcf05ade
ohjuyoungg <ejrm12@naver.com>
2024-03-21 03:00:45 +0900

    feat: requestLine inner 클래스 구현 메소드 분리 및 역할별 리팩토링

--- src/main/java/webserver/httpMessage/HttpRequest.java
+++ src/main/java/webserver/httpMessage/HttpRequest.java
@@ -1,34 +1,24 @@
 package webserver.httpMessage;
 
-import db.Database;
+import model.HttpMethod;
 import model.User;
-import utils.HttpRequestUtils;
-import java.io.IOException;
-import java.io.OutputStream;
+
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequest {
+    private final RequestLine requestLine;
+    private final Map<String, String> header;
+    private final Map<String, String> body;
 
-    private String url; // 인스턴스 변수로 변경
-
-    public HttpRequest(String url) {
-        this.url = url; // 생성자에서 초기화
+    public HttpRequest(String startLine, Map<String, String> header, Map<String, String> body) {
+        this.requestLine = new RequestLine(startLine);
+        this.header = header;
+        this.body = body;
     }
 
-    public boolean isCreateRequest() {
-        return url != null && url.startsWith("/create"); // null 체크 추가
-    }
-
-    public void registrationParamValue(OutputStream out, Map<String, String> params) throws IOException {
-        if (isCreateRequest()) { // isCreateRequest 메서드를 호출하여 null 체크
-            User user = createUserFromParams(params);
-            Database.addUser(user);
-            HttpResponse httpResponse = new HttpResponse();
-            httpResponse.response302Header(out);
-        }
-    }
-
-    public static User createUserFromParams(Map<String, String> params) {
+    public static User buildUser(Map<String, String> params) {
         String userId = params.get("userId");
         String password = params.get("password");
         String name = params.get("name");
@@ -36,9 +26,67 @@ public class HttpRequest {
         return new User(userId, password, name, email);
     }
 
-    private Map<String, String> parseParamsFromURL(String url) {
-        String[] parts = url.split("\\?");
-        String queryString = parts[1];
-        return HttpRequestUtils.parseQueryString(queryString);
+    public RequestLine getRequestLine() {
+        return requestLine;
+    }
+
+    public Map<String, String> getHeader() {
+        return header;
+    }
+
+    public Map<String, String> getBody() {
+        return body;
+    }
+
+    public static class RequestLine {
+        private final HttpMethod method;
+        private final String path;
+        private final Map<String, String> params;
+        private final String protocol;
+
+        public RequestLine(String startLine) {
+            String[] tokens = startLine.split(" ");
+            if (tokens.length != 3) {
+                throw new IllegalArgumentException("HTTP 요청 라인이 올바르지 않습니다.");
+            }
+            this.method = HttpMethod.of(tokens[0]);
+            this.path = getPath(tokens[1]);
+            this.params = getParams(tokens[1]);
+            this.protocol = tokens[2];
+        }
+
+        public HttpMethod getMethod() {
+            return method;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public Map<String, String> getParams() {
+            return params;
+        }
+
+        public String getProtocol() {
+            return protocol;
+        }
+
+        private String getPath(String requestTarget) {
+            return requestTarget.split("\\?")[0];
+        }
+
+        private Map<String, String> getParams(String requestTarget) {
+            String[] tokens = requestTarget.split("\\?");
+            if (tokens.length <= 1 || tokens[1].isBlank()) {
+                return Collections.emptyMap();
+            }
+            Map<String, String> params = new HashMap<>();
+            String[] queryParams = tokens[1].split("&");
+            for (String queryParam : queryParams) {
+                String[] entry = queryParam.split("=");
+                params.put(entry[0], entry[1]);
+            }
+            return params;
+        }
     }
 }
