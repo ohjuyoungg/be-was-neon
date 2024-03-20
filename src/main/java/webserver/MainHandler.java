8f1c750963bd66b15c4f9059534f04a3a84aae25
ohjuyoungg <ejrm12@naver.com>
2024-03-21 03:03:54 +0900

    feat: GET, POST 별 조건문을 통한 handlerMapping 메소드 구현

--- src/main/java/webserver/MainHandler.java
+++ src/main/java/webserver/MainHandler.java
@@ -2,14 +2,15 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
+import db.Database;
+import model.HttpMethod;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileSerializer;
 import utils.HttpRequestUtils;
-import utils.IOUtils;
 import webserver.httpMessage.HttpRequest;
 import webserver.httpMessage.HttpResponse;
 
@@ -18,8 +19,7 @@ public class MainHandler implements Runnable {
 
     private static final Logger log = LoggerFactory.getLogger(MainHandler.class);
     private final Socket connection;
-    private static String url;
-    private Map<String, String> parameters;
+    public static final String STATIC_PATH = "src/main/resources/static";
 
 
     public MainHandler(Socket connectionSocket) {
@@ -31,63 +31,52 @@ public class MainHandler implements Runnable {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (
+                InputStream in = connection.getInputStream();
+                OutputStream out = connection.getOutputStream()
+        ) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            HttpRequest httpRequest = new HttpRequest(readRequestLog(br, out));
+            HttpRequest httpRequest = HttpRequestUtils.parse(br);
             HttpResponse httpResponse = new HttpResponse();
 
-            httpRequest.registrationParamValue(out, parameters); // parameters 전달
+            handlerMapping(
+                    httpRequest.getRequestLine(),
+                    httpResponse,
+                    out
+            );
 
-
-            // 정적 파일 요청을 처리합니다.
-            File file = new File("src/main/resources/static" + url);
-            if (file.exists() && !file.isDirectory()) {
-                byte[] body = httpResponse.readFileContents(file);
-                ContentType contentType = new ContentType();
-                String type = contentType.getContentType(url);
-                httpResponse.response200Header(out, body, type);
-            } else {
-                // 파일이 존재하지 않는 경우 404 응답을 보냅니다.
-                log.error("File not found: " + url);
-                httpResponse.send404NotFound(out);
-            }
         } catch (IOException e) {
             log.error("Error handling request: {}", e.getMessage());
         }
     }
 
-    private String readRequestLog(BufferedReader br, OutputStream out) throws IOException {
-        String line = br.readLine();
-        String[] tokens = line.split(" ");
-        Map<String, String> headers = new HashMap<>();
+    private void handlerMapping(
+            HttpRequest.RequestLine requestLine,
+            HttpResponse response,
+            OutputStream out
+    ) throws IOException {
+        HttpMethod method = requestLine.getMethod();
+        String path = requestLine.getPath();
+        Map<String, String> params = requestLine.getParams();
 
-        String requestBody = "";
-        String queryString = "";
-
-        while (!"".equals(line)) {
-            log.debug(line);
-            line = br.readLine();
-            String[] headerTokens = line.split(": ", 2);
-
-            if (headerTokens.length == 2) {
-                headers.put(headerTokens[0], headerTokens[1]);
+        if (method == HttpMethod.GET) {
+            File file = new File(STATIC_PATH + path);
+            if (file.exists() && !file.isDirectory()) {
+                byte[] body = FileSerializer.toByteArray(file);
+                String type = ContentType.getContentType(path);
+                response.response200Header(out, body, type);
+            } else {
+                log.error("File not found: " + path);
+                response.send404NotFound(out);
             }
+        } else if (method == HttpMethod.POST && path.equals("/create")) {
+            User user = HttpRequest.buildUser(params);
+            Database.addUser(user);
+            response.response302Header(out);
+        } else {
+            // UNREACHABLE
+            log.warn(String.format("method=%s path=%s 는 처리할 수 없는 요청입니다.", method, path));
+            response.send404NotFound(out);
         }
-
-        if (tokens[1].contains("?")) {
-            queryString = tokens[1].substring(tokens[1].indexOf("?") + 1);
-            log.debug("query string:{}",queryString);
-        }
-
-        if (headers.get("Content-Length") != null) {
-            requestBody = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
-        }
-        queryString = queryString + "&" + requestBody;
-        parameters = HttpRequestUtils.parseQueryString(queryString);
-
-        log.info(Arrays.toString(tokens));
-        url = tokens[1];
-
-        return url;
     }
 }
