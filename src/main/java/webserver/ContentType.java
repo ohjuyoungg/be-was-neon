9d00b3462acc485ca7173cc05de4866c1bdf1dbc
ohjuyoungg <ejrm12@naver.com>
2024-03-24 02:24:38 +0900

    refactor: switch 문 map 으로 리팩토링하여 변경

--- src/main/java/webserver/ContentType.java
+++ src/main/java/webserver/ContentType.java
@@ -1,28 +1,26 @@
 package webserver;
 
+import java.util.Map;
+
 public class ContentType {
+
+    private static final Map<String, String> CONTENT_TYPE = Map.of(
+            "html", "text/html;charset=utf-8",
+            "css", "text/css",
+            "js", "application/javascript",
+            "png", "image/png",
+            "jpg", "image/jpeg",
+            "jpeg", "image/jpeg",
+            "gif", "image/gif",
+            "svg", "image/svg+xml"
+    );
 
     public static String getContentType(String url) {
         String[] parts = url.split("\\.");
         String extension = parts[parts.length - 1];
-        switch (extension) {
-            case "html":
-                return "text/html;charset=utf-8";
-            case "css":
-                return "text/css";
-            case "js":
-                return "application/javascript";
-            case "png":
-                return "image/png";
-            case "jpg":
-            case "jpeg":
-                return "image/jpeg";
-            case "gif":
-                return "image/gif";
-            case "svg":
-                return "image/svg+xml";
-            default:
-                return "application/octet-stream";
-        }
+        return CONTENT_TYPE.getOrDefault(
+                extension,
+                "application/octet-stream"
+        );
     }
 }
