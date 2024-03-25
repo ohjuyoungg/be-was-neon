1736a8fe7cb7c8b182e51a7aa26781eeb4a128a2
ohjuyoungg <ejrm12@naver.com>
2024-03-25 11:55:46 +0900

    refactor: 코드 구현에 따른 주석 적용

--- src/main/java/webserver/handler/MappingHandler.java
+++ src/main/java/webserver/handler/MappingHandler.java
@@ -21,6 +21,10 @@ public class MappingHandler {
     private static final Logger log = LoggerFactory.getLogger(MappingHandler.class);
     public static final String STATIC_PATH = "src/main/resources/static";
 
+    /**
+     * 주어진 HTTP 요청에 대해 적절한 처리를 수행하도록 지정된
+     * 요청 핸들러 메서드를 호출합니다.
+     */
     public void handleRequest(HttpRequest request, HttpResponse response, OutputStream out) throws IOException {
         HttpMethod method = request.getRequestLine().getMethod();
         String path = request.getRequestLine().getPath();
@@ -34,6 +38,11 @@ public class MappingHandler {
         }
     }
 
+    /**
+     * GET 요청에 대한 처리를 수행합니다.
+     * 요청된 파일이 존대하면 해당 파일을 응답으로 전송하고,
+     * 존재하지 않으면 404 응답을 반환합니다.
+     */
     private void handleGetRequest(String path, HttpResponse response, OutputStream out) throws IOException {
         File file = new File(STATIC_PATH + path);
         if (file.exists() && !file.isDirectory()) {
@@ -44,6 +53,11 @@ public class MappingHandler {
         }
     }
 
+    /**
+     * POST 요청에 대한 처리를 수행합니다.
+     * '/create' 경로에 대해서는 새로운 사용자를 생성하고,
+     * '/login' 경로에 대해서는 사용자 로그인을 처리합니다.
+     */
     private void handlePostRequest(String path, HttpRequest request, HttpResponse response, OutputStream out) throws IOException {
         if (path.equals("/create")) {
             handleUserCreate(request, response, out);
@@ -55,6 +69,11 @@ public class MappingHandler {
         }
     }
 
+    /**
+     * 사용자 생성 요청을 처리합니다.
+     * 요청된 사용자 정보를 데이터베이스에 추가하고,
+     * 응답으로 302 코드를 반환합니다.
+     */
     private void handleUserCreate(HttpRequest request, HttpResponse response, OutputStream out) throws IOException {
         User user = HttpRequest.buildUser(request.getBody());
         Database.addUser(user);
@@ -62,6 +81,11 @@ public class MappingHandler {
         response.response302Header(out);
     }
 
+    /**
+     * 사용자 로그인 요청을 처리합니다.
+     * 요청된 사용자 정보를 검증하고, 쿠키를 설정하여
+     * 로그인 상태를 유지합니다.
+     */
     private void handleUserLogin(HttpRequest request, HttpResponse response, OutputStream out) throws IOException {
         Map<String, String> body = request.getBody(); // Request의 body를 가져옴
         String userId = body.get("userId");
@@ -81,11 +105,19 @@ public class MappingHandler {
         }
     }
 
+    /**
+     * 지원되지 않는 HTTP 메소드에 대한 요청을 처리합니다.
+     * 해당 경로와 메소드에 대한 404 응답을 반환합니다.
+     */
     private void handleUnsupportedRequest(HttpMethod method, String path, HttpResponse response, OutputStream out) throws IOException {
         log.warn(String.format("Unsupported request: method=%s, path=%s", method, path));
         response.send404NotFound(out);
     }
 
+    /**
+     * 정적 파일을 서빙하기 위한 메소드입니다.
+     * 요청된 파일의 내용과 유형에 따라 적절한 응답을 생성합니다.
+     */
     private void serveStaticFile(File file, HttpResponse response, OutputStream out) throws IOException {
         byte[] body = FileUtil.toByteArray(file);
         String type = ContentType.getContentType(file.getName());
---
+++ src/test/java/webserver/httpRequest/HttpRequestTest.java
@@ -0,0 +1,2 @@
+package webserver.httpRequest;public class httpRequestTest {
+}
